c295d34f87888a7c40558fb2fe37b540306befa4
Matt-C-G <matthewgraha4302@gmail.com>
2025-09-18 10:31:00 -0400

    chore: bump version to v0.4.5

--- quietpatch/__init__.py
+++ quietpatch/__init__.py
@@ -1,5 +1,5 @@
 """QuietPatch - Privacy-first vulnerability scanner with deterministic HTML reports."""
 
-__version__ = "0.4.4"
-__build_tag__ = "v0.4.4"  # This will be updated by CI
+__version__ = "0.4.5"
+__build_tag__ = "v0.4.5"  # This will be updated by CI
 __db_snapshot_date__ = None  # This will be populated at runtime if available
